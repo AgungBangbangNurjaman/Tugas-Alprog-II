b4d116f1def181f683b920c5e4efe0dc13a1d157
Agung BangBang Nurjaman <agungprofane@gmail.com>
2017-03-27 20:42:07 -0700

    Add files via upload

---
+++ TugasElerning.cpp
@@ -0,0 +1,34 @@
+//Sub-Program menggunakan Fungsi 
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void inputdata();
+void Hitung();
+
+main()	{
+cout<<"________________________________________\n";
+cout<<"Menghitung Gaji Pekerja                 |\n";
+cout<<"Tunjangan Sebesar 10% dari Gaji Pokok   |\n";
+cout<<"________________________________________|\n";
+inputdata();
+Hitung();
+getch();
+}
+	void inputdata() {
+	char nama[30],alamat[30];
+	cout<<"Masukkan Nama : ";
+	gets(nama);
+	cout<<"Masukan Alamat: ";
+	gets(alamat);
+}
+	void Hitung(){
+	int Gajipok,tunjangan,total;
+	cout<<"Masukan Gaji Pokok = ";
+	cin>>Gajipok;
+	tunjangan=0.1*Gajipok;
+	total=Gajipok+tunjangan;
+	cout<<"Total Gaji yang Diterima Adalah "<<total;
+}
